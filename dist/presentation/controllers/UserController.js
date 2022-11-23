585a44bb8091981578e6167211349de9a44e95f2
Henrique da Silva Cardoso <henrique.silva@warren.com.br>
2022-11-23 08:59:00 -0300

    feat: now dist is supposedly removed

--- dist/presentation/controllers/UserController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserController = void 0;
-class UserController {
-    static handle(res) {
-        res.send('A rota para se testar o funcionamento é localhost:3000/customerk tanto POST quanto GET');
-    }
-}
-exports.UserController = UserController;
