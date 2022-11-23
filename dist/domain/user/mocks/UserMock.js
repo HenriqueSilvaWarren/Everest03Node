dfdcd068f7765d6454361c7ad4897b5f83b6718b
Henrique da Silva Cardoso <henrique.silva@warren.com.br>
2022-11-23 10:24:56 -0300

    refactor: dist won't be going to remote repository anymore

--- dist/domain/user/mocks/UserMock.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Users = void 0;
-exports.Users = [];
--- dist/presentation/controllers/UserController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserController = void 0;
-class UserController {
-    static handle(req, res) {
-        res.send('A rota para se testar o funcionamento é localhost:3000/customer tanto POST quanto GET');
-    }
-}
-exports.UserController = UserController;
