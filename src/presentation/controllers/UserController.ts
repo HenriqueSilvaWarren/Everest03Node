562780a3a6a3aea582e7780e3793a63394ec510f
Henrique da Silva Cardoso <henrique.silva@warren.com.br>
2022-12-01 08:28:56 -0300

    refactor: changed import

--- src/presentation/controllers/UserController.ts
+++ src/presentation/controllers/UserController.ts
@@ -1,8 +1,8 @@
-import express = require("express");
+import { Request, Response } from "express";
 import { IUserController } from "@controlInterfaces/IUserController";
 
 export class UserController implements IUserController {
-    handle(req: express.Request, res: express.Response): void {
+    handle(req: Request, res: Response): void {
         res.send('A rota para se testar o funcionamento é localhost:3000/customer tanto POST quanto GET');
     }
 }
