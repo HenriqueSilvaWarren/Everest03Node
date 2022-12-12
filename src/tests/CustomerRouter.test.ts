34a8778b571133c89d557224024b68025ff401b7
Henrique da Silva Cardoso <113135850+HenriqueSilvaWarren@users.noreply.github.com>
2022-12-12 15:51:17 -0300

    Update src/tests/CustomerRouter.test.ts
    Co-authored-by: Tandy <93944728+tandy-souza@users.noreply.github.com>

--- src/tests/CustomerRouter.test.ts
+++ src/tests/CustomerRouter.test.ts
@@ -3,7 +3,7 @@ import { CustomerRouter } from "../presentation/routers/CustomerRouter"
 
 describe('CustomerRouter', () => {
     it('Should return a router when setup is called', () => {
-        const costumerRouter = customContainer.resolve(CustomerRouter);
+        const customerRouter = customContainer.resolve(CustomerRouter);
 
         const router = costumerRouter.setup();
 
